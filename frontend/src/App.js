4c468ffc692ae4d9c1cd028ef8127b8de89d8a2e
vasantsc <vasantchimmalagi121@gmail.com>
2024-02-08 12:35:26 +0530

    initialised Frontend and Backend for project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header/>
+      <footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/header/header.js
@@ -0,0 +1,10 @@
+
+function header() {
+  return (
+    <div>
+      header
+    </div>
+  )
+}
+
+export default header
